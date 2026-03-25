02564bc13fd78a875189f1654f40971144600a1e
boeani05 <flyingdoge@gmx.at>
2026-03-25 16:49:09 +0100

    automatically created test class for the main class

---
+++ src/test/java/com/boeani/bloggingAPI/BloggingApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.boeani.bloggingAPI;
+
+import com.boeani.bloggingAPI.controller.PostController;
+import com.boeani.bloggingAPI.service.PostService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+@SpringBootTest
+class BloggingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
