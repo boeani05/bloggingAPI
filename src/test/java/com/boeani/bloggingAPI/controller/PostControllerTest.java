e095225bb97de2af81fbc46ae9388e342eb01db7
boeani05 <flyingdoge@gmx.at>
2026-04-08 17:02:14 +0200

    added tests, that check, if endpoint can be reached and if  fields in json are set

--- src/test/java/com/boeani/bloggingAPI/controller/PostControllerTest.java
+++ src/test/java/com/boeani/bloggingAPI/controller/PostControllerTest.java
@@ -1,10 +1,14 @@
 package com.boeani.bloggingAPI.controller;
 
+import com.boeani.bloggingAPI.dto.request.CreatePostRequest;
+import com.boeani.bloggingAPI.entity.Post;
 import com.boeani.bloggingAPI.service.PostService;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -29,5 +33,56 @@ class PostControllerTest {
         mockMvc
                 .perform(MockMvcRequestBuilders.get("/posts"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void shouldReturn201WhenCreatingPost() throws Exception {
+        Post post = new Post();
+
+        String jsonInput = """
+                {
+                "title": "",
+                "content": "",
+                "category": ""
+                }
+                """;
+
+        Mockito.when(postService.createPost(Mockito.any(CreatePostRequest.class))).thenReturn(post);
+
+        mockMvc
+                .perform(MockMvcRequestBuilders.post("/posts").contentType(MediaType.APPLICATION_JSON).content(jsonInput))
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+
+    @Test
+    public void shouldReturnCorrectPostFieldsWhenCreatingPost() throws Exception {
+        String title = "Title Test";
+        String content = "Content Test";
+        String category = "Category Test";
+
+        Post post = new Post();
+        post.setTitle(title);
+        post.setContent(content);
+        post.setCategory(category);
+
+        String jsonInput = String.format("""
+                        {
+                        "title": "%s",
+                        "content": "%s",
+                        "category": "%s"
+                        }
+                        """,
+                title,
+                content,
+                category
+        );
+
+        Mockito.when(postService.createPost(Mockito.any(CreatePostRequest.class))).thenReturn(post);
+
+        mockMvc
+                .perform(MockMvcRequestBuilders.post("/posts").contentType(MediaType.APPLICATION_JSON).content(jsonInput))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.title", Matchers.is(title)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content", Matchers.is(content)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.category", Matchers.is(category)));
     }
 }
