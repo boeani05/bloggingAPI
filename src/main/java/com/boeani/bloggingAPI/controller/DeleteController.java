c0b32a376402e47d5b5e6da8508ce7f30e7c251b
boeani05 <flyingdoge@gmx.at>
2026-04-22 17:13:43 +0200

    Update DeleteController.java

--- src/main/java/com/boeani/bloggingAPI/controller/DeleteController.java
+++ src/main/java/com/boeani/bloggingAPI/controller/DeleteController.java
@@ -7,15 +7,31 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * REST controller for deleting posts.
+ */
 @RestController
 public class DeleteController {
 
+    /**
+     * Service that performs delete operations.
+     */
     private final PostService postService;
 
+    /**
+     * Creates a new controller instance.
+     *
+     * @param postService service used to process delete requests
+     */
     public DeleteController(PostService postService) {
         this.postService = postService;
     }
 
+    /**
+     * Deletes a post by id.
+     *
+     * @param id identifier of the post to delete
+     */
     @DeleteMapping("/posts/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletePostById(
