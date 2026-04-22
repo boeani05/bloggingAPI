7e4d254b0dff0008040e6baea84405ff3f780523
boeani05 <flyingdoge@gmx.at>
2026-04-22 17:14:01 +0200

    Update PostNotFoundException.java

--- src/main/java/com/boeani/bloggingAPI/exceptions/PostNotFoundException.java
+++ src/main/java/com/boeani/bloggingAPI/exceptions/PostNotFoundException.java
@@ -1,6 +1,14 @@
 package com.boeani.bloggingAPI.exceptions;
 
+/**
+ * Thrown when a requested post does not exist.
+ */
 public class PostNotFoundException extends RuntimeException {
+    /**
+     * Creates a new exception with a custom message.
+     *
+     * @param message error description
+     */
     public PostNotFoundException(String message) {
         super(message);
     }
