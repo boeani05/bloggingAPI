73e6920441af0b51ccce83f36a0d2f5d2de99491
boeani05 <flyingdoge@gmx.at>
2026-04-22 17:09:42 +0200

    created an exception that is thrown, when the tags of the post are not entered

---
+++ src/main/java/com/boeani/bloggingAPI/exceptions/TagsNotEnteredException.java
@@ -0,0 +1,7 @@
+package com.boeani.bloggingAPI.exceptions;
+
+public class TagsNotEnteredException extends RuntimeException {
+    public TagsNotEnteredException(String message) {
+        super(message);
+    }
+}
