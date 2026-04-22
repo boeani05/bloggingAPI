4b14e035ca88fb9b977c0f723af2fc0bf456ec77
boeani05 <flyingdoge@gmx.at>
2026-04-22 17:14:06 +0200

    Update TagsNotEnteredException.java

--- src/main/java/com/boeani/bloggingAPI/exceptions/TagsNotEnteredException.java
+++ src/main/java/com/boeani/bloggingAPI/exceptions/TagsNotEnteredException.java
@@ -1,6 +1,14 @@
 package com.boeani.bloggingAPI.exceptions;
 
+/**
+ * Thrown when tags are missing in a post request.
+ */
 public class TagsNotEnteredException extends RuntimeException {
+    /**
+     * Creates a new exception with a custom message.
+     *
+     * @param message error description
+     */
     public TagsNotEnteredException(String message) {
         super(message);
     }
