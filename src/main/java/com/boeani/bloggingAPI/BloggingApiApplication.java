264f23c0a8d4447c22e450386a12b453a5529802
boeani05 <flyingdoge@gmx.at>
2026-04-22 17:09:12 +0200

    Update BloggingApiApplication.java

--- src/main/java/com/boeani/bloggingAPI/BloggingApiApplication.java
+++ src/main/java/com/boeani/bloggingAPI/BloggingApiApplication.java
@@ -3,9 +3,17 @@ package com.boeani.bloggingAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Application bootstrap class for the Blogging API.
+ */
 @SpringBootApplication
 public class BloggingApiApplication {
 
+	/**
+	 * Starts the Spring Boot application context.
+	 *
+	 * @param args command-line arguments passed at startup
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(BloggingApiApplication.class, args);
 	}
