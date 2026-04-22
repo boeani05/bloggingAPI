16c2977db72db60cd6b09c63440094dbbf7f9514
boeani05 <flyingdoge@gmx.at>
2026-04-22 17:14:10 +0200

    Update TitleNotEnteredException.java

--- src/main/java/com/boeani/bloggingAPI/exceptions/TitleNotEnteredException.java
+++ src/main/java/com/boeani/bloggingAPI/exceptions/TitleNotEnteredException.java
@@ -1,6 +1,14 @@
 package com.boeani.bloggingAPI.exceptions;
 
+/**
+ * Thrown when a title is missing in a post request.
+ */
 public class TitleNotEnteredException extends RuntimeException {
+    /**
+     * Creates a new exception with a custom message.
+     *
+     * @param message error description
+     */
     public TitleNotEnteredException(String message) {
         super(message);
     }
