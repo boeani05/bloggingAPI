cbc164cecc2101971dd9f2d03b0a9d81cd4de305
boeani05 <flyingdoge@gmx.at>
2026-04-22 17:08:18 +0200

    created an exception, when the content of the post is not entered

---
+++ src/main/java/com/boeani/bloggingAPI/exceptions/ContentNotEnteredException.java
@@ -0,0 +1,7 @@
+package com.boeani.bloggingAPI.exceptions;
+
+public class ContentNotEnteredException extends RuntimeException {
+    public ContentNotEnteredException(String message) {
+        super(message);
+    }
+}
