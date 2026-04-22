a739ea6fe0db708908cc746e71f9a9d575d963d1
boeani05 <flyingdoge@gmx.at>
2026-04-22 17:13:47 +0200

    Update CategoryNotEnteredException.java

--- src/main/java/com/boeani/bloggingAPI/exceptions/CategoryNotEnteredException.java
+++ src/main/java/com/boeani/bloggingAPI/exceptions/CategoryNotEnteredException.java
@@ -1,6 +1,14 @@
 package com.boeani.bloggingAPI.exceptions;
 
+/**
+ * Thrown when category is missing in a post request.
+ */
 public class CategoryNotEnteredException extends RuntimeException {
+    /**
+     * Creates a new exception with a custom message.
+     *
+     * @param message error description
+     */
     public CategoryNotEnteredException(String message) {
         super(message);
     }
