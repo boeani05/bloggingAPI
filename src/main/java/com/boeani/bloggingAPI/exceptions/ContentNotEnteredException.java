4c606837ea3eaed1883cc7923900c8ce8b57c90f
boeani05 <flyingdoge@gmx.at>
2026-04-22 17:13:50 +0200

    Update ContentNotEnteredException.java

--- src/main/java/com/boeani/bloggingAPI/exceptions/ContentNotEnteredException.java
+++ src/main/java/com/boeani/bloggingAPI/exceptions/ContentNotEnteredException.java
@@ -1,6 +1,14 @@
 package com.boeani.bloggingAPI.exceptions;
 
+/**
+ * Thrown when content is missing in a post request.
+ */
 public class ContentNotEnteredException extends RuntimeException {
+    /**
+     * Creates a new exception with a custom message.
+     *
+     * @param message error description
+     */
     public ContentNotEnteredException(String message) {
         super(message);
     }
