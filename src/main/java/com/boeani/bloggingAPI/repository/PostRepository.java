3932cf8c8bff153407a998dfdfd00e651a40d293
boeani05 <flyingdoge@gmx.at>
2026-04-29 15:07:47 +0200

    repo can search for a term to find specific posts

--- src/main/java/com/boeani/bloggingAPI/repository/PostRepository.java
+++ src/main/java/com/boeani/bloggingAPI/repository/PostRepository.java
@@ -3,6 +3,8 @@ package com.boeani.bloggingAPI.repository;
 import com.boeani.bloggingAPI.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Persistence interface for {@link Post} entities.
  * <p>
@@ -10,4 +12,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * </p>
  */
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findDistinctByTitleContainingIgnoreCaseOrContentContainingIgnoreCaseOrCategoryContainingIgnoreCase(
+            String titleTerm,
+            String contentTerm,
+            String categoryTerm
+    );
 }
